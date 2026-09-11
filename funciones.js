6519e57859eb3df26b9ab96bce23bfa83ebbdebe
javierlizarraga3005-sielo <javierlizarraga3005@gmail.com>
2026-09-10 20:16:47 -0700

    Fix gin image filename in bebidas object

--- funciones.js
+++ funciones.js
@@ -1,6 +1,6 @@
 const bebidas = {
     mezcalitas: "WhatsApp image 2026-09-10 at 5.50.42 PM.jpeg",
-    gin: "47d9c764-9be2-bee5-8fd0f30378e7.jpg",
+    gin: "47d9c764-9be2-4432-bee5-8fd0f30378e7.jpg",
     carajillo: "eee17077-7f6c-493b-9707-3070d3fcc720.jpg",
     coqueta: "dff8842b-a26e-42af-9b8f-9b019e100768.jpg",
     lychee: "bada8892-55bc-4672-bad1-61727f8fb0ed.jpg"

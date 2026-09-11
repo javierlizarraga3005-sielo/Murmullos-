2c0d5b3a230d9dc9897f535d586df2a05b3692a3
javierlizarraga3005-sielo <javierlizarraga3005@gmail.com>
2026-09-10 20:19:03 -0700

    Fix typo in mezcalitas image filename

--- funciones.js
+++ funciones.js
@@ -1,5 +1,5 @@
 const bebidas = {
-    mezcalitas: "WhatsApp image 2026-09-10 at 5.50.42 PM.jpeg",
+    mezcalitas: "WhatsApp Image 2026-09-10 at 5.50.42 PM.jpeg",
     gin: "47d9c764-9be2-4432-bee5-8fd0f30378e7.jpg",
     carajillo: "eee17077-7f6c-493b-9707-3070d3fcc720.jpg",
     coqueta: "dff8842b-a26e-42af-9b8f-9b019e100768.jpg",
